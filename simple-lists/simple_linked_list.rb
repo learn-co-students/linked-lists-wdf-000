cfed59fa4ef2b9043f1813b63f00027e8fb21e69
ozPop <ozziepopovas@gmail.com>
2016-11-30 13:00:56 -0500

    refactor solution

--- simple-lists/simple_linked_list.rb
+++ simple-lists/simple_linked_list.rb
@@ -1,9 +1,9 @@
 class Node
-  attr_accessor :data, :next
+  attr_accessor :data, :next_node
 
   def initialize(data, next_node = nil)
     @data = data
-    @next = next_node
+    @next_node = next_node
   end
 end
 
@@ -14,51 +14,92 @@ class LinkedList
     @head = Node.new(data)
   end
 
+  # returns the last node in the LL
+  def tail
+    if self.head.next_node
+      node = self.head.next_node
+      while node.next_node
+        node = node.next_node
+      end
+      node
+    else
+      self.head
+    end
+  end
+
+  # takes an index and returns data at that index
   def index_at(index)
     pointer = head
     index.times do
-      return nil unless pointer.next
-      pointer = pointer.next
+      return nil unless pointer.next_node
+      pointer = pointer.next_node
     end
     pointer.data
   end
 
+  # inserts given data at given index
   def insert_at_index(index, data)
     pointer = self.head
     (1..index - 1).each do |number|
-      if pointer.next.nil? && (index != number)
-        pointer.next = Node.new(nil, nil)
+      if pointer.next_node.nil? && (index != number)
+        pointer.next_node = Node.new(nil, nil)
       end
-      pointer = pointer.next
+      pointer = pointer.next_node
     end
     if pointer
-      old_next = pointer.next
-      pointer.next = Node.new(data, old_next)
+      old_next = pointer.next_node
+      pointer.next_node = Node.new(data, old_next)
     end
   end
 
+  # adds a new node to the front of the LL (aka prepend)
   def unshift(data)
-    pointer = self.head
+    old_head = self.head
     self.head = Node.new(data)
-    self.head.next = pointer
+    self.head.next_node = old_head
   end
 
+  # removes a node from the front of the LL and returns it
+  def shift
+    binding.pry
+    shifted_node = self.head
+    self.head = self.head.next_node
+    shifted_node
+  end
+
+  # add to the end of the LL (aka append)
   def push(data)
-    pointer = head
-    while pointer.next
-      pointer = pointer.next
+    pointer = self.head
+    while pointer.next_node
+      pointer = pointer.next_node
     end
-    pointer.next = Node.new(data, nil)
+    pointer.next_node = Node.new(data, nil)
+  end
+
+  # removes a node from the end of the LL and returns it
+  # only pops if LL is > 2 elements in length
+  def pop
+    if self.head.next_node
+      node = self.head.next_node
+      while node.next_node.next_node
+        node = node.next_node
+      end
+      poppedNode = node.next_node
+      node.next_node = nil
+      return poppedNode
+    else
+      return nil
+    end
   end
 
   # https://www.sitepoint.com/ruby-interview-questions-linked-lists-and-hash-tables/
   def reverse(node)
-    return node if node.nil? || node.next.nil?
+    return node if node.nil? || node.next_node.nil?
 
-    next_node = node.next
-    new_head = reverse(node.next)
-    next_node.next = node
-    node.next = nil
+    next_node = node.next_node
+    new_head = reverse(node.next_node)
+    next_node.next_node = node
+    node.next_node = nil
     new_head
   end
 end
