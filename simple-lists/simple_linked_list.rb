8d1fdcef762e3312c284b5b00ecfeab6f6503542
ozPop <ozziepopovas@gmail.com>
2016-12-01 13:30:30 -0500

    update solution

--- simple-lists/simple_linked_list.rb
+++ simple-lists/simple_linked_list.rb
@@ -27,28 +27,109 @@ class LinkedList
     end
   end
 
-  # takes an index and returns data at that index
-  def index_at(index)
-    pointer = head
-    index.times do
-      return nil unless pointer.next_node
-      pointer = pointer.next_node
+  # returns total number of nodes in LL (includes head in the count)
+  def size
+    if self.head.next_node
+      node = self.head
+      i = 1
+      while node.next_node
+        node = node.next_node
+        i += 1
+      end
+      return i
+    else
+      return 1
     end
-    pointer.data
   end
 
-  # inserts given data at given index
-  def insert_at_index(index, data)
-    pointer = self.head
-    (1..index - 1).each do |number|
-      if pointer.next_node.nil? && (index != number)
-        pointer.next_node = Node.new(nil, nil)
+  # returns true if passed data is in the LL else false
+  def contains?(data)
+    node = self.head
+    while node.next_node
+      if node.data == data
+        return true
+      else
+        node = node.next_node
       end
-      pointer = pointer.next_node
     end
-    if pointer
-      old_next = pointer.next_node
-      pointer.next_node = Node.new(data, old_next)
+    return false
+  end
+
+  # returns the index of the node containing specified data, else nil
+  def find(data)
+    index = 0
+    current_node = self.head
+    loop do
+      if current_node.data == data
+        return index
+      else
+        break if current_node.next_node.nil?
+        current_node = current_node.next_node
+        index += 1
+      end
+    end
+    return nil
+  end
+
+  # takes an index and returns data at that index
+  def index_at(index)
+    current_node = head
+    index.times do
+      return nil unless current_node.next_node
+      current_node = current_node.next_node
+    end
+    current_node.data
+  end
+
+  def to_s
+    output = ''
+    current_node = self.head
+    until current_node.nil?
+      output += '(' + current_node.data.to_s + ') -> '
+      current_node = current_node.next_node
+    end
+    output += 'nil'
+  end
+
+  # ALTER LinkedList
+
+  # inserts given data at given index
+  # currently does not insert at head index
+  def insert_at_index(index, data)
+    current_node = self.head
+    (1..index - 1).each do |number|
+      if current_node.next_node.nil? && (index != number)
+        current_node.next_node = Node.new(nil, nil)
+      end
+      current_node = current_node.next_node
+    end
+    if current_node
+      old_next = current_node.next_node
+      current_node.next_node = Node.new(data, old_next)
+    end
+  end
+
+  # remove a node at given index, update links of nodes
+  def remove_at_index(index)
+    return "LL contains only head node" if self.size == 1
+    if index == 0
+      self.shift
+    else
+      i = 1
+      current_node = self.head.next_node
+      previous_node = self.head
+      loop do
+        break if current_node.nil?
+        if index == i
+          previous_node.next_node = current_node.next_node
+          return 'Node removed'
+        else
+          previous_node = current_node
+          current_node = current_node.next_node
+          i += 1
+        end
+      end
+      return 'Index not found'
     end
   end
 
@@ -61,7 +142,6 @@ class LinkedList
 
   # removes a node from the front of the LL and returns it
   def shift
-    binding.pry
     shifted_node = self.head
     self.head = self.head.next_node
     shifted_node
@@ -69,27 +149,26 @@ class LinkedList
 
   # add to the end of the LL (aka append)
   def push(data)
-    pointer = self.head
-    while pointer.next_node
-      pointer = pointer.next_node
+    current_node = self.head
+    while current_node.next_node
+      current_node = current_node.next_node
     end
-    pointer.next_node = Node.new(data, nil)
+    current_node.next_node = Node.new(data, nil)
   end
 
   # removes a node from the end of the LL and returns it
-  # only pops if LL is > 2 elements in length
   def pop
-    if self.head.next_node
-      node = self.head.next_node
-      while node.next_node.next_node
-        node = node.next_node
-      end
-      poppedNode = node.next_node
-      node.next_node = nil
-      return poppedNode
-    else
-      return nil
+    return "Can't delete head" if self.size == 1
+    current_node = self.head
+    old_node = nil
+    while current_node.next_node
+      old_node = current_node
+      current_node = current_node.next_node
     end
+    tail = current_node
+    # "pop" the tail
+    old_node.next_node = nil
+    return tail
   end
 
   # https://www.sitepoint.com/ruby-interview-questions-linked-lists-and-hash-tables/
